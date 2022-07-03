146766677fdb0507281386502422a9a4f3451ba0
Ivaylo Stamov <ivaylo.stamov@mentormate.com>
2022-07-03 14:15:23 +0300

    Create Person class

---
+++ BasicCrudApplicaition/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicCrudApplicaition.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string City { get; set; } = string.Empty;
+    }
+}

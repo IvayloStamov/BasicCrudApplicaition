527b3700b333cf4daabdf620722c6add57db5aca
Ivaylo Stamov <ivaylo.stamov@mentormate.com>
2022-07-03 16:45:22 +0300

    Adding Get(Id), Update, Delete and Add methods

--- BasicCrudApplicaition/Controllers/PersonController.cs
+++ BasicCrudApplicaition/Controllers/PersonController.cs
@@ -1,6 +1,7 @@
 ﻿using BasicCrudApplicaition.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BasicCrudApplicaition.Controllers
 {
@@ -8,18 +9,70 @@ namespace BasicCrudApplicaition.Controllers
     [ApiController]
     public class PersonController : ControllerBase
     {
+         private static List<Person> people = new List<Person>()
+         {
+             new Person()
+             {
+                 Id = 1, FirstName = "Martin", LastName = "Indzhov",
+                 Age = 25, City = "Stara Zagora"
+             }
+         };
+
         [HttpGet]
-        public async Task<ActionResult<List<Person>>> Get()
+        public async Task<ActionResult<List<Person>>> GetAll()
         {
-            var people = new List<Person>()
-            {
-                new Person()
-                {
-                    Id = 1, FirstName = "Martin", LastName = "Indzhov",
-                    Age = 25, City = "Stara Zagora"
-                }
-            };
             return Ok(people);  
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Person>> Get(int Id)
+        {
+            Person person = people.Find(x => x.Id == Id);
+            if(person != null)
+            {
+                return Ok(person);  
+            }
+            return BadRequest($"No Person with an Id {Id} found.");
+        }
+
+         
+        [HttpPost]
+        public async Task<ActionResult<List<Person>>> Add(Person input)
+        {
+
+            
+            if(people.Any(x => x.Id == input.Id))
+            {
+                return BadRequest($"There is already a person with the Id {input.Id}");
+            }          
+            
+            people.Add(input);
+            return Ok(people);
+
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult<List<Person>>> Delete(int Id)
+        {
+            var person = people.Find(x => x.Id == Id);
+            if (person != null)
+            {
+                people.Remove(person);
+                return Ok(people);
+            }
+            return BadRequest($"No Person with an Id {Id} found.");
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Person>> Update(Person input)
+        {
+            var person = people.Find(x => x.Id == input.Id);
+            person.FirstName = input.FirstName;
+            person.LastName = input.LastName;
+            person.Age = input.Age;
+            person.City = input.City;
+
+            return Ok(person);
         }
     }
 }
